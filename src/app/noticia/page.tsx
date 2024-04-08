97fb5e4408a5888a0d4a6b75ae736047638c58de
samirsouzasys <samir.guitar@gmail.com>
2024-04-08 12:23:24 -0300

    fix: ajusta layout

--- src/app/noticia/page.tsx
+++ src/app/noticia/page.tsx
@@ -10,7 +10,6 @@ export default function Page() {
   const titulo: string = searchParams.get("titulo") ?? ""
   const introducao: string | null = searchParams.get("introducao")
   const data_publicacao: string | null = searchParams.get("data_publicacao")
-  const editorias: string | null = searchParams.get("editorias")
   const imagens: string = searchParams.get("imagens") ?? ""
 
   const inicioURLImagemInicio = imagens
@@ -28,33 +27,51 @@ export default function Page() {
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>IBGE Notícias</h1>
-      <h2>Notícia</h2>
-      <p>{titulo}</p>
+      <h1 className="text-2xl font-bold text-center mb-4 ">IBGE Notícias</h1>
+      <h2 className="text-2xl font-bold text-center mb-4 ">Notícia</h2>
+      <p className="font-bold">{titulo}</p>
       <br />
       <br />
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <ul>
-          <li>Introdução: {introducao}</li>
-          <li>Data publicação: {data_publicacao}</li>
-          <li>Editorias: {editorias}</li>
-          <Image
-            src={`https://${domain}/${imageIntroURL}`}
-            width={320}
-            height={180}
-            alt="imagem da notícia"
-          ></Image>
-          <li>
-            <Link href={link} target="_blank" rel="noopener noreferrer">
-              Ler Mais no site oficial!
-            </Link>
-          </li>
-          <li>
-            <Link href={"/"} target="_self" rel="noopener noreferrer">
-              Voltar para a tela inicial
-            </Link>
-          </li>
-        </ul>
+        <div className="mb-4">
+          <p className="font-bold">Introdução:</p>
+          {introducao}
+        </div>
+        <div className="mb-4">
+          <p className="font-bold">Introdução:</p>
+          {introducao}
+        </div>
+        <div className="mb-4">
+          <span className="font-bold">Data publicação: </span>
+          {data_publicacao}
+        </div>
+        <Image
+          src={`https://${domain}/${imageIntroURL}`}
+          width={320}
+          height={180}
+          alt="imagem da notícia"
+          className="mb-10"
+        />
+        <div>
+          <Link
+            href={link}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-xs text-indigo-500 hover:text-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 block   font-bold mb-2"
+          >
+            Ler Mais no site oficial!
+          </Link>
+        </div>
+        <div>
+          <Link
+            href={"/"}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-xs text-indigo-500 hover:text-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 block   font-bold mb-2"
+          >
+            Voltar para a tela inicial
+          </Link>
+        </div>
       </div>
     </main>
   )
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -34,18 +34,21 @@ interface INoticiaItem {
 
 const apiAddress = "http://servicodados.ibge.gov.br/api/v3/noticias"
 
-const fetchNoticias = async (paginaAtual: number = 1, tituloPesquisa: string = ''): Promise<INoticias> => {
+const fetchNoticias = async (
+  paginaAtual: number = 1,
+  tituloPesquisa: string = ""
+): Promise<INoticias> => {
   const qtd = 3
   const page = paginaAtual
 
-  return await fetch(apiAddress + "?page=" + page + "&qtd=" + qtd+ "&busca=" + tituloPesquisa).then((res) =>
-    res.json()
-  )
+  return await fetch(
+    apiAddress + "?page=" + page + "&qtd=" + qtd + "&busca=" + tituloPesquisas
+  ).then((res) => res.json())
 }
 
 export default function Home() {
   const [page, setPage] = useState(1)
-  const [busca, setBusca] = useState('')
+  const [busca, setBusca] = useState("")
   const router = useRouter()
 
   const {
@@ -55,7 +58,7 @@ export default function Home() {
     isPlaceholderData,
   } = useQuery<INoticias>({
     queryKey: ["INoticiaItem", page, busca],
-    queryFn: () => fetchNoticias(page,busca),
+    queryFn: () => fetchNoticias(page, busca),
     placeholderData: keepPreviousData,
   })
 
@@ -70,104 +73,128 @@ export default function Home() {
     router.push(`/noticia?${queryString}`)
   }
 
-  const {
-    register,
-    handleSubmit,
-  } = useForm({
+  const { register, handleSubmit } = useForm({
     defaultValues: {
       tituloBuscar: "",
-    }
-  });
+    },
+  })
 
   if (isLoading) return <div>Loading...</div>
   if (isError) return <div>Error: Falha ao buscar os dados</div>
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1 className="text-2xl font-bold text-center mb-4 ">IBGE Notícias</h1>
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <h1>IBGE Notícias</h1>
+        <span className="block text-sm font-bold text-gray-700 mb-2">
+          Página atual: {page}
+        </span>
+
+        <span className="block text-sm font-bold text-gray-700 mb-2">
+          Total de páginas: {noticias?.totalPages}
+        </span>
+        <br />
+        <button
+          className="middle none center rounded-lg bg-pink-500 py-3 px-6 font-sans text-xs font-bold uppercase text-white shadow-md shadow-pink-500/20 transition-all hover:shadow-lg hover:shadow-pink-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+          onClick={() => {
+            setPage(1)
+          }}
+          // Desabilita o botão "Primeira" se já tiver na primeira página
+          disabled={page == 1}
+        >
+          Primeira
+        </button>
+
+        <button
+          className="middle none center rounded-lg bg-pink-500 py-3 px-6 font-sans text-xs font-bold uppercase text-white shadow-md shadow-pink-500/20 transition-all hover:shadow-lg hover:shadow-pink-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+          onClick={() => setPage((old) => Math.max(old - 1, 0))}
+          disabled={page === 1}
+        >
+          Anterior
+        </button>
+
+        <button
+          className="middle none center rounded-lg bg-pink-500 py-3 px-6 font-sans text-xs font-bold uppercase text-white shadow-md shadow-pink-500/20 transition-all hover:shadow-lg hover:shadow-pink-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+          onClick={() => {
+            if (!isPlaceholderData && noticias?.nextPage) {
+              setPage((old) => old + 1)
+            }
+          }}
+          // Desabilita o botão "Próximo" até saber se há uma próxima página disponível
+          disabled={isPlaceholderData || !noticias?.nextPage}
+        >
+          Próximo
+        </button>
+
+        <button
+          className="middle none center rounded-lg bg-pink-500 py-3 px-6 font-sans text-xs font-bold uppercase text-white shadow-md shadow-pink-500/20 transition-all hover:shadow-lg hover:shadow-pink-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+          onClick={() => {
+            setPage(noticias?.totalPages ? noticias?.totalPages : 1)
+          }}
+          // Desabilita o botão "última" se já tiver na última página
+          disabled={noticias?.totalPages == page}
+        >
+          Última
+        </button>
       </div>
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <div>
-          <span>Página atual: {page}</span>
-          {' - '}
-          <span>Total de páginas: {noticias?.totalPages}</span>
-          <br />
-          <button
-            onClick={() => {
-              setPage(1)
-            }}
-            // Desabilita o botão "Primeira" se já tiver na primeira página
-            disabled={page == 1}
-          >
-            Primeira
-          </button>
-          {' - '}
-          <button
-            onClick={() => setPage((old) => Math.max(old - 1, 0))}
-            disabled={page === 1}
-          >
-            Anterior
-          </button>{" "}
-          {' - '}
-          <button
-            onClick={() => {
-              if (!isPlaceholderData && noticias?.nextPage) {
-                setPage((old) => old + 1)
-              }
-            }}
-            // Desabilita o botão "Próximo" até saber se há uma próxima página disponível
-            disabled={isPlaceholderData || !noticias?.nextPage}
-          >
-            Próximo
-          </button>
-          {' - '}
-          <button
-            onClick={() => {
-              setPage(noticias?.totalPages ? noticias?.totalPages : 1)
-            }}
-            // Desabilita o botão "última" se já tiver na última página
-            disabled={noticias?.totalPages == page}
-          >
-            Última
-          </button>
-        </div>
-      </div>
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex mt-8">
         <form
           onSubmit={handleSubmit((data) => {
             setPage(1)
             setBusca(data.tituloBuscar)
           })}
         >
-          <label>Buscar pelo título</label>
-          <input {...register("tituloBuscar")} />
-          <input type="submit" value="Buscar!"/>
+          <label className="block text-sm font-bold text-gray-700 mb-2">
+            Buscar pelo título
+          </label>
+          <input
+            className="shadow-sm rounded-md w-full px-3 py-2 border border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
+            placeholder="texto para pesquisa"
+            {...register("tituloBuscar")}
+          />
+          <input
+            type="submit"
+            value="Buscar!"
+            className="middle none center rounded-lg bg-pink-500 py-3 px-6 font-sans text-xs font-bold uppercase text-white shadow-md shadow-pink-500/20 transition-all hover:shadow-lg hover:shadow-pink-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+          />
         </form>
       </div>
 
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <ul>
-          {noticias?.items.slice(0, 10).map((item: INoticiaItem) => (
-            <>
-              <li key={item.id}>
-                <strong>{item.titulo}</strong>
-                <p>{item.introducao}</p>
-                <button type="button" onClick={() => buildQueryParams(item)}>
-                  Ler Mais aqui mesmo!
-                </button>
-                <Link
-                  href={item.link}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  Ler Mais no site oficial!
-                </Link>
-              </li>
-              <br />
-            </>
-          ))}
-        </ul>
+      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex mt-8">
+        {noticias?.items.slice(0, 10).map((item: INoticiaItem) => (
+          <div className="mb-8" key={item.id}>
+            <div className="mb-4">
+              <strong>{item.titulo}</strong>
+              <p
+                className="overflow: hidden; 
+white-space: nowrap; "
+              >
+                {item.introducao}
+              </p>
+            </div>
+            <div>
+              <button
+                type="button"
+                onClick={() => buildQueryParams(item)}
+                className="text-xs text-indigo-500 hover:text-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500
+                block  
+                font-bold
+                mb-2
+                "
+              >
+                Ler Mais aqui mesmo!
+              </button>
+              <Link
+                href={item.link}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-xs text-indigo-500 hover:text-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 block   font-bold mb-2"
+              >
+                Ler Mais no site oficial!
+              </Link>
+            </div>
+          </div>
+        ))}
       </div>
     </main>
   )
