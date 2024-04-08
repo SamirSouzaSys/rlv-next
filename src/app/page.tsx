c6608b4db96fe9f3e55718af662ecfe363ee25b4
samirsouzasys <samir.guitar@gmail.com>
2024-04-08 09:32:58 -0300

    feat: adiciona FetchData, Paginacao, links - ler mais interno e externo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,147 @@
-import Image from "next/image";
+'use client'
+
+import { keepPreviousData, useQuery } from "@tanstack/react-query"
+import Link from "next/link"
+import { useRouter } from "next/navigation"
+import { useState } from "react"
+
+interface INoticias {
+  count: number
+  page: number
+  totalPages: number
+  nextPage: number
+  previousPage: number
+  showingFrom: number
+  showingTo: number
+  items: [INoticiaItem]
+}
+interface INoticiaItem {
+  id: number
+  tipo: string
+  titulo: string
+  introducao: string
+  data_publicacao: string
+  produto_id: number
+  produtos: string
+  editorias: string
+  imagens: string
+  produtos_relacionados: string
+  destaque: boolean
+  link: string
+  [key: string]: string | number | boolean
+}
+
+const apiAddress = "http://servicodados.ibge.gov.br/api/v3/noticias"
+
+const fetchNoticias = async (paginaAtual: number = 1): Promise<INoticias> => {
+  const qtd = 5
+  const page = paginaAtual
+  return await fetch(apiAddress + "?page=" + page + "&qtd=" + qtd).then((res) =>
+    res.json()
+  )
+}
 
 export default function Home() {
+  const [page, setPage] = useState(1)
+  const router = useRouter()
+
+  const {
+    data: noticias,
+    isLoading,
+    isError,
+    isPlaceholderData,
+  } = useQuery<INoticias>({
+    queryKey: ["INoticiaItem", page],
+    queryFn: () => fetchNoticias(page),
+    placeholderData: keepPreviousData,
+  })
+
+  if (isLoading) return <div>Loading...</div>
+  if (isError) return <div>Error: Falha ao buscar os dados</div>
+
+  const buildQueryParams = (item: INoticiaItem) => {
+    const queryString = Object.keys(item)
+      .map(
+        (key) =>
+          `${encodeURIComponent(key)}=${encodeURIComponent(String(item[key]))}`
+      )
+      .join("&")
+
+    router.push(`/noticia?${queryString}`)
+  }
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+        <h1>IBGE Notícias</h1>
+      </div>
+      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+        <div>
+          <span>Página atual: {page}</span>
+          <br />
+          <button
+            onClick={() => {
+              setPage(1)
+            }}
+            // Desabilita o botão "Primeira" se já tiver na primeira página
+            disabled={page == 1}
           >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+            Primeira
+          </button>
+          <br/>
+          <button
+            onClick={() => setPage((old) => Math.max(old - 1, 0))}
+            disabled={page === 1}
+          >
+            Anterior
+          </button>{" "}
+          <br/>
+          <button
+            onClick={() => {
+              if (!isPlaceholderData && noticias?.nextPage) {
+                setPage((old) => old + 1)
+              }
+            }}
+            // Desabilita o botão "Próximo" até saber se há uma próxima página disponível
+            disabled={isPlaceholderData || !noticias?.nextPage}
+          >
+            Próximo
+          </button>
+          <br/>
+          <button
+            onClick={() => {
+              setPage(noticias?.totalPages ? noticias?.totalPages : 1)
+            }}
+            // Desabilita o botão "última" se já tiver na última página
+            disabled={noticias?.totalPages == page}
+          >
+            Última
+          </button>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+        <ul>
+          {noticias?.items.slice(0, 10).map((item: INoticiaItem) => (
+            <>
+              <li key={item.id}>
+                <strong>{item.titulo}</strong>
+                <p>{item.introducao}</p>
+                <button type="button" onClick={() => buildQueryParams(item)}>
+                  Ler Mais aqui mesmo!
+                </button>
+                <Link
+                  href={item.link}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Ler Mais no site oficial!
+                </Link>
+              </li>
+              <br />
+            </>
+          ))}
+        </ul>
       </div>
     </main>
-  );
+  )
 }
